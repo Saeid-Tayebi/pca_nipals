9f54cebcdf8d193ba6df5c3692826a493b7120a0
Saeid <s.saeid.tayebi@gmail.com>
2024-09-19 18:17:20 -0400

    plot optimized

--- Python/code_executor.py
+++ Python/code_executor.py
@@ -14,18 +14,19 @@ Beta=np.array([3,2,1])
 Y=(X@Beta.T).reshape(-1,1)
 color_map_data =  Y.reshape(-1) #color map should contain 1D data
 # Set parameters
-Num_com = 3            # Number of PLS components (=Number of X Variables)
+Num_com = 2            # Number of PLS components (=Number of X Variables)
 alpha = 0.95            # Confidence limit (=0.95)
 X_test=np.array([[0.9,0.1,0.2],[0.5 , 0.4 , 0.9]])
 scores_pca=np.array([1,2])
 # Model implementation as a Module
+'''
 pca_model=pca_m.pca_nipals(X,Num_com,alpha)
 
 x_hat,T_score,Hotelin_T2,SPE_X=pca_m.pca_evaluation(pca_model,X_test)
 print(f'x_hat={x_hat}\n',f'T_score={T_score}\n',f'Hotelin_T2={Hotelin_T2}\n',f'SPE_X={SPE_X}\n')
 
 pca_m.visual_plot(pca_model,scores_pca,X_test,color_map_data) # 
-
+'''
 #%% 
 # Model implementation as a Class
 MyPcaModel=pca_c()
@@ -34,8 +35,8 @@ MyPcaModel.train(X,Num_com,alpha)
 x_hat,T_score,Hotelin_T2,SPE_X=MyPcaModel.evaluation(X_test)
 print(f'x_hat={x_hat}\n',f'T_score={T_score}\n',f'Hotelin_T2={Hotelin_T2}\n',f'SPE_X={SPE_X}\n')
 
-MyPcaModel.visual_plot(scores_pca,X_test,color_map_data)
-
+MyPcaModel.visual_plot(scores_pca,X_test,color_map_data,data_labeling=True)
+input('')
 
 
 # %%
